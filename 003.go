f9604e45ba9350ccf962101ea555f9b48ef87caf
Kimberly Knudtson <kimberly.di.knudtson@gmail.com>
2022-08-18 14:06:51 -0600

    serialize works

--- 003.go
+++ 003.go
@@ -24,6 +24,44 @@ func main() {
 //node = Node('root', Node('left', Node('left.left')), Node('right'))
 //assert deserialize(serialize(node)).left.left.val == 'left.left'
 
+//Make some rules...
+//start at leftmost leaf or rightmost leaf - doesn't matter - choose leftmost
+//at a node serialize(node) gives ("self","left","right")
+//serialize(root.left.left) = "(left.left,nil,nil)" = this.val+serialize(this.left)+serialize(this.right)
+//serialize(root.left) = "(left,(left.left,nil,nil),nil" = this.val+serialize(this.left)+serilize(this.right)
+//serialize(root.right) = "(right,nil,nil)" =this.val+serialize(this.left)+serialize(this.right)
+//serialize(root) = "(root,(left,(left.left,nil,nil),nil),(right,nil,nil))"
+
 func serialize(node Node) string {
-	return node.left.left.val
+	s := node.val + ","
+	if node.left != nil {
+		s = s + serialize(*node.left) + ","
+	} else {
+		s = s + "nil" + ","
+	}
+	if node.right != nil {
+		s = s + serialize(*node.right)
+	} else {
+		s = s + "nil"
+	}
+	return "(" + s + ")"
 }
+
+//root := Node{val: "root"}
+//root.left = &Node{val: "left"}
+//root.left.left = &Node{val: "left.left"}
+//root.right = &Node{val: "right"}
+//serialize(root) = "(root,(left,(left.left,nil,nil),nil),(right,nil,nil))"
+
+//ds("(root,(left,(LEFTNODE),nil),(right,nil,nil))", leftnode)
+//ds("(root,(LEFTNODE),(right,nil,nil))")
+func deserialize(s string, leftnode Node, rightnode Node) Node {
+	//format is (self,left,right)
+}
+
+func main() {
+	s := "root"
+	root := deserialize(s)
+	fmt.Print(s)
+
+}*/
